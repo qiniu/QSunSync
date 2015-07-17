0c4ac2eb90c6091b21bbcffc2e7d6aefb904c74e
jemygraw <jemygraw@gmail.com>
2015-07-17 16:32:24 +0800

    Improve the account setting logic and check the ak sk validity.

--- SunSync/SunSync/AccountSettingPage.xaml.cs
+++ SunSync/SunSync/AccountSettingPage.xaml.cs
@@ -5,6 +5,10 @@ using System.Windows.Input;
 using Newtonsoft.Json;
 using SunSync.Models;
 using System.IO;
+using System.Threading;
+using Qiniu.Util;
+using Qiniu.Storage;
+using Qiniu.Storage.Model;
 namespace SunSync
 {
     /// <summary>
@@ -20,17 +24,25 @@ namespace SunSync
             this.loadAccountInfo();
         }
 
+        private void BackToHome_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.mainWindow.GotoHomePage();
+        }
+
+        /// <summary>
+        /// view my ak & sk button click handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ViewMyAKSK_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             string myAKSKLink = "https://portal.qiniu.com/setting/key";
             System.Diagnostics.Process.Start(myAKSKLink);
         }
 
-        private void BackToHome_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            this.mainWindow.GotoHomePage();
-        }
-
+        /// <summary>
+        /// load account settings from local file if exists
+        /// </summary>
         private void loadAccountInfo()
         {
             string myDocPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
@@ -50,19 +62,23 @@ namespace SunSync
                 }
                 catch (Exception)
                 {
-                    //todo
+                    //todo error log
                 }
             }
         }
 
-        private void AccountSettingChange_EventHandler(object sender, TextChangedEventArgs e)
+        /// <summary>
+        /// save account settings to local file and check the validity of the settings
+        /// </summary>
+        private void SaveAccountSetting(object accountObj)
         {
-            string accessKey = this.AccessKeyTextBox.Text.Trim();
-            string secretKey = this.SecretKeyTextBox.Text.Trim();
-            Account acc = new Account();
-            acc.AccessKey = accessKey;
-            acc.SecretKey = secretKey;
-            string accData = JsonConvert.SerializeObject(acc);
+            Account account = (Account)accountObj;
+            //overwrite memory settings
+            SystemConfig.ACCESS_KEY = account.AccessKey;
+            SystemConfig.SECRET_KEY = account.SecretKey;
+
+            //write settings to local file
+            string accData = JsonConvert.SerializeObject(account);
             string myDocPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string appDir = System.IO.Path.Combine(myDocPath, "qsunbox");
             try
@@ -79,10 +95,60 @@ namespace SunSync
             }
             catch (Exception)
             {
-                //todo
+                //todo error log
+
+                Dispatcher.Invoke(new Action(delegate
+                {
+                    this.SettingsErrorTextBlock.Text = "帐号设置写入文件失败";
+                }));
             }
-            SystemConfig.ACCESS_KEY = accessKey;
-            SystemConfig.SECRET_KEY = secretKey;
+
+            //check ak & sk validity
+            Mac mac = new Mac(account.AccessKey, account.SecretKey);
+            BucketManager bucketManager = new BucketManager(mac);
+            StatResult statResult = bucketManager.stat("NONE_EXIST_BUCKET", "NONE_EXIST_KEY");
+            if (statResult.ResponseInfo.isNetworkBroken())
+            {
+                Dispatcher.Invoke(new Action(delegate
+                {
+                    this.SettingsErrorTextBlock.Text = "网络故障";
+                }));
+            }
+            else
+            {
+                if (statResult.ResponseInfo.StatusCode == 401)
+                {
+                    Dispatcher.Invoke(new Action(delegate
+                    {
+                        this.SettingsErrorTextBlock.Text = "AK 或 SK 设置不正确";
+                    }));
+                }
+                else
+                {
+                    Dispatcher.Invoke(new Action(delegate
+                    {
+
+                        this.SettingsErrorTextBlock.Text = "";
+                        this.mainWindow.GotoHomePage();
+                    }));
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// save account settings button click handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveAccountSettings_EventHandler(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string accessKey = this.AccessKeyTextBox.Text.Trim();
+            string secretKey = this.SecretKeyTextBox.Text.Trim();
+            Account account = new Account();
+            account.AccessKey = accessKey;
+            account.SecretKey = secretKey;
+            new Thread(new ParameterizedThreadStart(this.SaveAccountSetting)).Start(account);
         }
     }
 }
