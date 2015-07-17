e5cdb83c0b5ee4d084e39dc43ed2d66e1dd4ea7c
jemygraw <jemygraw@gmail.com>
2015-07-17 16:41:09 +0800

    Refractor the code of main window.

--- SunSync/SunSync/MainWindow.xaml.cs
+++ SunSync/SunSync/MainWindow.xaml.cs
@@ -26,103 +26,49 @@ namespace SunSync
             this.syncSettingPage = new SyncSettingPage(this);
             this.syncProgressPage = new SyncProgressPage(this);
             this.syncResultPage = new SyncResultPage(this);
-            this.loadAccountInfo();
-            this.loadRecentSyncJobs();
         }
 
-        private void loadAccountInfo()
-        {
-            string myDocPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string accPath = System.IO.Path.Combine(myDocPath, "qsunbox", "account.json");
-            if (File.Exists(accPath))
-            {
-                byte[] accData = null;
-                try
-                {
-                    using (FileStream fs = new FileStream(accPath, FileMode.Open, FileAccess.Read))
-                    {
-                        accData = new byte[fs.Length];
-                        fs.Read(accData, 0, (int)fs.Length);
-                    }
-                    Account acct = JsonConvert.DeserializeObject<Account>(Encoding.UTF8.GetString(accData));
-                    SystemConfig.ACCESS_KEY = acct.AccessKey;
-                    SystemConfig.SECRET_KEY = acct.SecretKey;
-                }
-                catch (Exception)
-                {
-                    //todo
-                }
-            }
-        }
-
-        private List<SyncRecord> loadRecentSyncJobs()
-        {
-            List<SyncRecord> syncRecords = new List<SyncRecord>();
-            string myDocPath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string jobsDir = System.IO.Path.Combine(myDocPath, "qsunbox", "jobs");
-            if (Directory.Exists(jobsDir))
-            {
-                string[] jobNamePaths = Directory.GetFiles(jobsDir);
-                foreach (string jobNamePath in jobNamePaths)
-                {
-                    string jobName = System.IO.Path.GetFileName(jobNamePath);
-                    try
-                    {
-                        string[] items = Encoding.UTF8.GetString(Convert.FromBase64String(jobName)).Split('\t');
-                        if (items.Length == 3)
-                        {
-                            string localDir = items[0];
-                            string targetBucket = items[1];
-                            string binaryDate = items[2];
-                            DateTime syncDate = DateTime.FromBinary(Convert.ToInt64(binaryDate));
-                            SyncRecord syncRecord = new SyncRecord();
-                            syncRecord.FilePath = jobNamePath;
-                            syncRecord.SyncLocalDir = localDir;
-                            syncRecord.SyncTargetBucket = targetBucket;
-                            syncRecord.SyncDateTime = syncDate;
-                            syncRecord.SyncDateTimeStr = syncDate.ToString("yyy-MM-dd HH:mm:ss");
-                            syncRecords.Add(syncRecord);
-                        }
-                    }
-                    catch (Exception) { }
-                }
-            }
-            syncRecords.Sort(new Comparison<SyncRecord>(delegate(SyncRecord a, SyncRecord b)
-            {
-                return (int)(b.SyncDateTime.Subtract(a.SyncDateTime).TotalSeconds);
-            }));
-            return syncRecords;
-        }
-
+        //default page
         private void MainWindow_Loaded_EventHandler(object sender, RoutedEventArgs e)
         {
             this.GotoHomePage();
         }
 
+        //go to home page
+        internal void GotoHomePage()
+        {
+            this.GotoQuickStartPage();
+        }
+
+        //go to account setting page
         internal void GotoAccountPage()
         {
             this.MainHostFrame.Content = this.accountSettingPage;
         }
 
-        internal void GotoHomePage()
+        //go to quick start page
+        internal void GotoQuickStartPage()
         {
-            List<SyncRecord> syncRecords = loadRecentSyncJobs();
+            List<SyncRecord> syncRecords = Tools.loadRecentSyncJobs();
             this.quickStartPage.LoadSyncRecords(syncRecords);
             this.MainHostFrame.Content = this.quickStartPage;
         }
 
+        //go to sync setting page
         internal void GotoSyncSettingPage(SyncSetting syncSetting)
         {
             this.MainHostFrame.Content = this.syncSettingPage;
             this.syncSettingPage.LoadSyncSetting(syncSetting);
         }
 
+        //go to sync progress page
         internal void GotoSyncProgress(Models.SyncSetting syncSetting)
         {
             this.MainHostFrame.Content = this.syncProgressPage;
             this.syncProgressPage.LoadSyncSetting(syncSetting);
         }
 
+        //go to sync result page
         internal void GotoSyncResultPage(TimeSpan spentTime, bool fileOverwrite, int fileExistsCount, string fileExistsLogPath, int fileOverwriteCount,
                string fileOverwriteLogPath, int fileNotOverwriteCount, string fileNotOverwriteLogPath, int fileUploadErrorCount, string fileUploadErrorLogPath,
                int fileUploadSuccessCount, string fileUploadSuccessLogPath)
